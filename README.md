5f81f48865bce552cec2e80e68a79c984f501b1d
bharratgehlot <161546117+bharratgehlot@users.noreply.github.com>
2025-11-09 13:18:50 +0530

    Initial commit: Project 27

---
+++ README.md
@@ -0,0 +1,11 @@
+# Project 27
+
+The code repository for Project 27 
+
+## Built with react, Built for React Developer {Name}
+
+
+## Release Date: 10 November 2025
+
+with a lot of love and care 
+
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+function App() {
+
+  return (
+    <>
+      
+      <h1>Project 27</h1>
+      
+      <p>
+        We are kickstarting Project 27 from 9-11-25 and will release tommarrow
+      </p>
+    </>
+  )
+}
+
+export default App
